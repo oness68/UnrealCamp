eec28ff6f9b8b936baeb6948ed59d6ab038df9b2
조진서 <oness68@naver.com>
2024-12-17 05:12:46 +0900

    몫 구하기

--- Programmers/Algorithm codekata/몫 구하기/main.cpp
+++ Programmers/Algorithm codekata/몫 구하기/main.cpp
@@ -1,18 +1,19 @@
 #include <string>
-//#include<iostream>
 #include <vector>
-
+//#include<iostream>
 using namespace std;
 
-
 int solution(int num1, int num2) {
-    int answer = num1*num2;
+    int answer = num1/num2;
     return answer;
 }
 
 //int main()
 //{
-//    cout << solution(3, 4);
+//    cout<<solution(10, 5);
+//
+//    cout << solution(7, 2);
+//
 //
 //    return 0;
 //}
