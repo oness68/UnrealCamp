08fcadb9d19ac5c96a86a1a7dff5f17002a06d91
조진서 <oness68@naver.com>
2024-12-17 04:46:36 +0900

    두 수의 곱

---
+++ Programmers/Algorithm codekata/두 수의 곱/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+
+int solution(int num1, int num2) {
+    int answer = num1*num2;
+    return answer;
+}
+
+int main()
+{
+    cout << solution(3, 4);
+
+    return 0;
+}
