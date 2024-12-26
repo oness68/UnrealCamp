07245d633b28c765f56671d2d953b9884d75f73c
조진서 <oness68@naver.com>
2024-12-26 19:34:25 +0900

    1

---
+++ C++/StudyC++/나이 출력/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+
+int solution(int n) {
+    int answer = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            answer += i;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    cout<< solution(10);
+}
+
